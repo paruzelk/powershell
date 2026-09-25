da1e470bbb28621d97a59161a2ce822198a2211d
paruzelk <paruzelk@gmail.com>
2026-09-25 12:48:35 +0300

    Import Legacy/ServerMaintenance_deep/Core/Context_.ps1 from JimsGarage

---
+++ Legacy/ServerMaintenance_deep/Core/Context_.ps1
@@ -0,0 +1,47 @@
+﻿#requires -Version 5.1
+
+
+function New-SMFContext
+{
+
+    param
+    (
+        [string]$RootPath,
+
+        [hashtable]$Config,
+
+        [object]$Environment
+    )
+
+
+    $Context = [PSCustomObject]@{
+
+
+        RootPath = $RootPath
+
+
+        Config = $Config
+
+
+        Environment = $Environment
+
+
+        Profile = $Config.Profile
+
+
+        StartTime = Get-Date
+
+
+        LogFile = $Global:SMFLogFile
+
+
+        Results = @()
+
+
+
+    }
+
+
+    return $Context
+
+}
