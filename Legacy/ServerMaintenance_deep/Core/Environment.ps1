5a9b78ee7802661418c4b34e4130f3bdaace9792
paruzelk <paruzelk@gmail.com>
2026-09-25 12:48:37 +0300

    Import Legacy/ServerMaintenance_deep/Core/Environment.ps1 from JimsGarage

---
+++ Legacy/ServerMaintenance_deep/Core/Environment.ps1
@@ -0,0 +1,31 @@
+﻿function Get-SMFEnvironment {
+
+
+    $os = Get-CimInstance Win32_OperatingSystem
+
+
+    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+
+    $principal = New-Object Security.Principal.WindowsPrincipal($identity)
+
+    $isAdmin = $principal.IsInRole(
+        [Security.Principal.WindowsBuiltInRole]::Administrator
+    )
+
+
+    return [PSCustomObject]@{
+
+        ComputerName = $env:COMPUTERNAME
+
+        OS = $os.Caption
+
+        PowerShell = $PSVersionTable.PSVersion.ToString()
+
+        Architecture = $os.OSArchitecture
+
+        Admin = $isAdmin
+
+    }
+
+
+}
